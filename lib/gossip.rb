3d47e78388e5377a90118f633d18a9327b7a658c
Neeiyo <louis.grandjean99@gmail.com>
2021-02-01 16:09:57 +0100

    premier jet

---
+++ lib/gossip.rb
@@ -0,0 +1,30 @@
+require 'bundler'
+Bundler.require
+
+class Gossip
+	attr_reader :author, :content
+
+	def initialize(author, content)
+		@author = author
+		@content = content
+	end
+
+	def save
+    CSV.open("db/gossip.csv", "a+") do |csv|
+    csv << [@author, @content]
+  	end 
+	end
+
+	def self.find(id)
+		return Gossip.all[id.to_i]
+	end
+
+	def self.all
+		all_gossips = []
+		CSV.read("./db/gossip.csv").each do |csv_line|
+		  all_gossips << Gossip.new(csv_line[0], csv_line[1])
+		end
+		return all_gossips
+	  end
+end
+	#binding.pry
